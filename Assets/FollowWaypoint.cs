d803430bb642c0fd151bfdb64f2403033994493c
ginger59620 <a59620@alunos.ipb.pt>
2025-10-15 17:51:37 +0100

    following a tracker part 1

--- Assets/FollowWaypoint.cs
+++ Assets/FollowWaypoint.cs
@@ -10,26 +10,39 @@ public class FollowWaypoint : MonoBehaviour
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
 
+    GameObject tracker;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ProgressTracker()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
             currentWP++;
 
         if (currentWP >= waypoints.Length)
             currentWP = 0;
 
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProgressTracker();
+
         //this.transform.LookAt(waypoints[currentWP].transform);
-        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+       // Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
 
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
 
-        this.transform.Translate(0, 0, speed * Time.deltaTime);
+        //this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
